d2e700e93f2215bcee55610e5f0d2f28eaaab509
zolanjoeeee <65596062+zolanjoeeee@users.noreply.github.com>
2020-05-19 14:47:27 +0200

    Create theme.css

---
+++ base.js/theme.css
@@ -0,0 +1,111 @@
+body{
+	font-family: Segoe, "Segoe UI", "DejaVu Sans", "Trebuchet MS", Verdana, "sans-serif";
+	font-size: large;
+	background: #282C35;
+	background-image: url(bgimg_recolor.png);
+	background-size: auto;
+	background-repeat: no-repeat;
+	background-position: center;
+	margin: 0px;
+	padding: 0px;
+}
+div{
+	box-sizing:border-box;
+	margin: 0px;
+	padding: 0px;
+}
+h1, h2{
+	font-stretch: expanded;
+	font-style: normal;
+	font-weight: normal;
+}
+@keyframes slideFromTop{
+	0% {transform: translateY(-100%);}
+	100% {transform: translateY(0);}
+}
+@keyframes slideFromLeft{
+	0% {transform: translateX(-100%);}
+	100% {transform: translateX(0);}
+}
+#divBody{
+	position: relative;
+	z-inde: 5;
+	padding: 5px;
+	padding-top: 10px;
+	background: #C7D1E8;
+	box-shadow: 0px 0px 30px 0px rgba(0, 0, 0, 0.3);
+	animation: 0.7s ease-out 0s 1 slideFromTop;
+}
+#divBodyWrapper{
+	position: relative;
+	z-index: 1;
+}
+#divHeaderWrapper{
+	position: relative;
+	background: #5A7DCD;
+	margin: 0px;
+	padding-bottom: 5px;
+	box-shadow: 0px 5px 3px 0px rgba(0, 0, 0, 0.3);
+	z-index: 15;
+}
+#tableHeader{
+	font-size: x-large
+}
+.tablinks{
+	background-color: transparent;
+	border: none;
+	font-size: x-large;
+}
+.centered{
+	margin-left: auto;
+	margin-right: auto;
+	text-align: center;
+}
+.bodyWidth{
+	width: 50%
+}
+.headerElem{
+	position: relative
+}
+.headerElem:hover:before{
+	visibility: visible;
+	transform: scaleX(1);
+}
+.headerElem:before{
+	content: "";
+ 	position: absolute;
+	width: 100%;
+	height: 2px;
+	bottom: 0;
+	left: 0;
+	background-color: #000;
+	visibility: hidden;
+	transform: scaleX(0);
+	transition: all 0.3s ease-in-out 0s;
+}
+.tabcontent {
+	animation: 0.7s ease-out 0s 1 slideFromTop;
+	position: relative;
+	z-index: 5;
+	padding: 5px;
+	padding-top: 10px;
+	background: #C7D1E8;
+	box-shadow: 0px 0px 30px 0px rgba(0, 0, 0, 0.3);
+}
+
+.footer {
+	position: fixed;
+	left: 0;
+	bottom: 0;
+	text-align: right;
+	font-size: small;
+	background: #5A7DCD;
+}
+.divProj{
+	padding: 25px 0px;
+}
+.title {
+	background: #9DB2E1;
+	margin: 0px;
+	font-size: larger;
+}
